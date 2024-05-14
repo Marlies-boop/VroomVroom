a9b5d76ad48e6fbb1259e2e9d289e83ff56cfd63
Rocka2012 <159764466+Rocka2012@users.noreply.github.com>
2024-05-14 15:31:36 +0200

    14/5/2024 push

--- VroomVroom/Assets/Scripts/Car/CarControllerTemp.cs
+++ VroomVroom/Assets/Scripts/Car/CarControllerTemp.cs
@@ -3,31 +3,118 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(InputManTemp))]
+[RequireComponent(typeof(CarInput))]
 public class CarControllerTemp : MonoBehaviour
 {
-    public InputManTemp inputmanager;
+    public CarInput inputmanager;
+    public Rigidbody rb;
+    public float Speed;
     public List<WheelCollider> throttlewheels;
-    public List<WheelCollider> steeringwheels;
+    public List<GameObject> steeringwheels;
     public List<GameObject> meshes;
+    public List<Light> lights;
+    public WheelCollider WheelL, WheelR;
     public float strength = 20000f;
     public float maxturn = 20f;
+    public float antiroll = 5000.0f;
+    public float brakeTorque = 1000f;
     void Start()
     {
-        inputmanager = GetComponent<InputManTemp>();
+        rb = GetComponent<Rigidbody>();
+        inputmanager = GetComponent<CarInput>();
+    }
+
+    private void Update()
+    {
+        if (inputmanager.lightValue)
+        {
+            Headlights();
+        }
     }
 
     void FixedUpdate()
     {
+        Speed = rb.velocity.magnitude * 3.6f;
+        print(Speed);
         foreach (WheelCollider wheel in throttlewheels)
         {
-            wheel.motorTorque = strength * Time.deltaTime * inputmanager.throttle;
+            wheel.motorTorque = strength * Time.deltaTime * inputmanager.driveValue;
         }
 
         foreach (GameObject wheel in steeringwheels)
         {
-            wheel.GetComponent<WheelCollider>steerAngle = maxturn * inputmanager.steering;
-            wheel.transform.localEulerAngles = new Vector3(0f, inputmanager.steering + maxturn, 0f);
+            wheel.GetComponent<WheelCollider>().steerAngle = maxturn * inputmanager.steerValue;
+            wheel.transform.localEulerAngles = new Vector3(0f, inputmanager.steerValue + maxturn, 0f);
+        }
+        foreach (GameObject mesh in meshes)
+        {
+            var parent = mesh.GetComponentInParent<WheelCollider>();
+            UpdateWheelPos(parent, mesh.transform);
+        }
+
+        if (inputmanager.brakeValue == true)
+        {
+            foreach (WheelCollider wheel in throttlewheels)
+            {
+                wheel.brakeTorque = brakeTorque;
+            }
+        }
+        else
+        {
+            foreach (WheelCollider wheel in throttlewheels)
+            {
+                wheel.brakeTorque = 0f;
+            }
+        }
+
+        void UpdateWheelPos(WheelCollider collider, Transform transform)
+        {
+            Vector3 pos = transform.position;
+            Quaternion quat = transform.rotation;
+
+            collider.GetWorldPose(out pos, out quat);
+
+            transform.position = pos;
+            transform.rotation = quat;
+        }
+
+        WheelHit hit = new WheelHit();
+        float travelL = 1.0f;
+        float travelR = 1.0f;
+
+        bool groundedL = WheelL.GetGroundHit(out hit);
+
+        if (groundedL)
+        {
+            travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y * WheelL.radius) / WheelL.suspensionDistance;
+            
+        }
+
+        bool groundedR = WheelR.GetGroundHit(out hit);
+
+        if (groundedR)
+        {
+            travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y * WheelR.radius) / WheelR.suspensionDistance;
+
+        }
+
+        var antirollForce = (travelL - travelR) * antiroll;
+
+        if (groundedL)
+        {
+            rb.AddForceAtPosition(WheelL.transform.up * -antirollForce, WheelL.transform.position);
+        }
+        if (groundedR)
+        {
+            rb.AddForceAtPosition(WheelR.transform.up * antirollForce, WheelR.transform.position);
+        }
+    }
+
+    public void Headlights()
+    {
+        foreach (Light light in lights)
+        {
+            light.intensity = light.intensity == 0 ? 2 : 0 ;
         }
     }
 }
